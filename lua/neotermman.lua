acb8e54a67b464f6524f5bdcf3cd3054a3a4ba36
Bhanuka <bhanukamax@gmail.com>
2020-12-22 23:10:57 +0530

    added comments

--- lua/neotermman.lua
+++ lua/neotermman.lua
@@ -1,23 +1,16 @@
-
 local win, term_buf, floating_term_buf
---local function set_mapping()
-    --local mappings = {
-        --q = 'close_window()',
-        --w = 'close_window()',
-    --}
 
-
-    --for k, v in pairs(mappings) do
-        --vim.api.nvim_buf_set_keymap(term_buf, 'n', k, ':lua require"neotermman".'..v..'<cr>', {
-            --nowait = true, noremap = true, silent = true
-        --})
-    --end
-
---end
+-- For closing the term buf in the attached mode
 local function go_back()
     vim.cmd(":bp")
 end
 
+-- For closing the term buf in floating mode
+local function close_window()
+    vim.api.nvim_win_close(win, true)
+end
+
+-- Attach mode key mappings
 local function set_mapping()
     local mappings = {
         q = 'go_back()',
@@ -30,6 +23,7 @@ local function set_mapping()
     end
 end
 
+-- Floating mode key mappings
 local function set_float_mapping()
     local mappings = {
         q = 'close_window()',
@@ -43,19 +37,14 @@ local function set_float_mapping()
     end
 end
 
-
-local function close_window()
-    --print("Trying to close")
-    --vim.api.nvim_win_close(win, true)
-    vim.api.nvim_win_close(win, true)
-end
-
+-- Get a reference to the first term buffer on the available buffers
+-- returns a table with the buffer name and the number
+-- { name, number }
 local function get_existing_term_buf()
     local found_buf = nil
     local buf_list = vim.api.nvim_list_bufs()
     for k,v in pairs(buf_list) do
         local buf_name = vim.api.nvim_buf_get_name(v)
-        --print(buf_name)
         if string.match(buf_name, "term://") then
             found_buf = {number = v, name = buf_name}
             
@@ -64,6 +53,7 @@ local function get_existing_term_buf()
     return found_buf
 end
 
+-- Opens the terminal in the current window
 local function attach_term()
     term_buf =  get_existing_term_buf()
     if term_buf then
@@ -76,6 +66,7 @@ local function attach_term()
     set_mapping()
 end
 
+-- Opens the terminal in a new floating window
 local function open_floating_term()
     term_buf =  get_existing_term_buf()
     local is_new_term_buf = false
@@ -127,8 +118,6 @@ local function open_floating_term()
 
     set_float_mapping()
 end
-
-
 
 return {
     close_window = close_window,
