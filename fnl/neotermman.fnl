7b36ec114fa9e2d84c621f89974d5bfae5f48a77
Bhanuka <bhanukamax@gmail.com>
2022-07-16 01:37:19 +0530

    fix

--- fnl/neotermman.fnl
+++ fnl/neotermman.fnl
@@ -1,6 +1,5 @@
-(local M {})
-(set M.is-term-open false)
-(set M.win-id nil)
+(var is-term-open false)
+(var win-id nil)
 
 (local harpoon (require :harpoon.term))
 
@@ -9,32 +8,29 @@
 
 (fn api [cmd]
   (. vim.api cmd))
-(set M.open-term
-     (fn []
-       (nvim-cmd :sp)
-       (nvim-cmd "wincmd J")
-       (let [current-win (vim.api.nvim_get_current_win)]
-         (set M.win-id current-win)
-         (vim.api.nvim_win_set_height current-win 10)
-         (harpoon.gotoTerminal 1)
-         (nvim-cmd "set nobuflisted"))))
 
-(set M.close-term
-     (fn []
-       (if
-        (vim.api.nvim_call_function :win_gotoid {1 M.win-id})
-        (vim.api.nvim_win_close M.win-id {}))))
+(fn open-term []
+  (nvim-cmd :sp)
+  (nvim-cmd "wincmd J")
+  (let [current-win (vim.api.nvim_get_current_win)]
+    (set win-id current-win)
+    (vim.api.nvim_win_set_height current-win 10)
+    (harpoon.gotoTerminal 1)
+    (nvim-cmd "set nobuflisted")))
 
-(set M.toggle-term
-     (fn []
-       (set M.is-term-open (not M.is-term-open))
-       (if win-id
-           (let [has-term (vim.api.nvim_win_is_valid M.win-id)]
-             (if has-term
-                 (close-term)
-                 (open-term)))
-           (open-term))))
-(vim.keymap.set :n :<leader>tt M.toggle-term)
-(set M.test 4)
-(print "loaded neotermman")
-M
+(fn close-term []
+  (if
+    (vim.api.nvim_call_function :win_gotoid {1 win-id})
+    (vim.api.nvim_win_close win-id {})))
+
+(fn toggle-term []
+  (set is-term-open (not is-term-open))
+  (if win-id
+    (let [has-term (vim.api.nvim_win_is_valid win-id)]
+      (if has-term
+        (close-term)
+        (open-term)))
+    (open-term)))
+
+(vim.keymap.set :n :<leader>to toggle-term)
+(vim.keymap.set :n :<leader>tt toggle-term)
--- lua/neotermman.lua
+++ lua/neotermman.lua
@@ -1,6 +1,5 @@
-local M = {}
-M["is-term-open"] = false
-M["win-id"] = nil
+local is_term_open = false
+local win_id = nil
 local harpoon = require("harpoon.term")
 local function nvim_cmd(cmd)
   return vim.api.nvim_command(cmd)
@@ -8,39 +7,34 @@ end
 local function api(cmd)
   return vim.api[cmd]
 end
-local function _1_()
+local function open_term()
   nvim_cmd("sp")
   nvim_cmd("wincmd J")
   local current_win = vim.api.nvim_get_current_win()
-  M["win-id"] = current_win
+  win_id = current_win
   vim.api.nvim_win_set_height(current_win, 10)
   harpoon.gotoTerminal(1)
   return nvim_cmd("set nobuflisted")
 end
-M["open-term"] = _1_
-local function _2_()
-  if vim.api.nvim_call_function("win_gotoid", {M["win-id"]}) then
-    return vim.api.nvim_win_close(M["win-id"], {})
+local function close_term()
+  if vim.api.nvim_call_function("win_gotoid", {win_id}) then
+    return vim.api.nvim_win_close(win_id, {})
   else
     return nil
   end
 end
-M["close-term"] = _2_
-local function _4_()
-  M["is-term-open"] = not M["is-term-open"]
-  if __fnl_global__win_2did then
-    local has_term = vim.api.nvim_win_is_valid(M["win-id"])
+local function toggle_term()
+  is_term_open = not is_term_open
+  if win_id then
+    local has_term = vim.api.nvim_win_is_valid(win_id)
     if has_term then
-      return __fnl_global__close_2dterm()
+      return close_term()
     else
-      return __fnl_global__open_2dterm()
+      return open_term()
     end
   else
-    return __fnl_global__open_2dterm()
+    return open_term()
   end
 end
-M["toggle-term"] = _4_
-vim.keymap.set("n", "<leader>tt", M["toggle-term"])
-M.test = 4
-print("loaded neotermman")
-return M
+vim.keymap.set("n", "<leader>to", toggle_term)
+return vim.keymap.set("n", "<leader>tt", toggle_term)
