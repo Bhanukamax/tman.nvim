b9252e641e4a668aed4cb777da0f9c71d161148d
Bhanuka <bhanukamax@gmail.com>
2022-07-23 23:56:27 +0530

    feat: enable multiple terminals

--- test.lua
+++ test.lua
@@ -1,4 +1,11 @@
 local tman = require("neotermman")
 print(vim.inspect(tman))
 print("test")
-return vim.keymap.set("n", "<leader>tt", tman.toggle)
+local function _1_()
+  return tman.toggle(1)
+end
+vim.keymap.set("n", "<leader>tt", _1_)
+local function _2_()
+  return tman.toggle(2)
+end
+return vim.keymap.set("n", "<leader>t2", _2_)
