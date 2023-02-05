69dc2f16578eb909be4c970124f9fc907fbb1404
Don Bhanuka <bhanukamax@gmail.com>
2023-02-05 18:29:41 +0530

    open/close terminal and send command

--- lua/tman.lua
+++ lua/tman.lua
@@ -1,14 +1,36 @@
+local len = function (tbl)
+    local i = 1
+    for _ in ipairs(tbl) do
+        i = i + 1
+    end
+
+    return i
+end
+
 local M = {}
 
 local tman = {}
 
+local pprint = function (tbl)
+   vim.pretty_print(tbl)
+end
+
 -- Goal is to drop harpoon as a dependency
 M.debug = function ()
-    vim.pretty_print(tman)
+    pprint(tman)
 end
 
 -- opens the bottom terminal
 M.openTerm = function ()
+    if tman.buf then
+    vim.cmd[[
+    sp
+    wincmd J
+    ]]
+        vim.api.nvim_set_current_buf(tman.buf)
+        return
+    end
+
     vim.cmd[[
     sp
     wincmd J
@@ -16,10 +38,21 @@ M.openTerm = function ()
     ]]
     tman.win = vim.api.nvim_get_current_win()
     tman.buf = vim.api.nvim_win_get_buf(tman.win)
+    tman.term = vim.b.terminal_job_id
 end
 
+M.sendCommand = function()
+    if tman.buf then
+        vim.api.nvim_chan_send(tman.term, "ls\n")
+    end
+end
 
 M.closeTerm = function()
+    local win_list = vim.api.nvim_list_wins()
+    if (len(win_list) <= 2)  then
+        return
+    end
+
     for _,v in pairs(vim.api.nvim_list_wins()) do
         local cbuf = vim.api.nvim_win_get_buf(v)
         if tman.buf ==  cbuf then
@@ -31,9 +64,7 @@ end
 M.goToTerm = function ()
     local win_id = vim.api.nvim_get_current_win()
     if tman.buf == nil then
-        vim.cmd[[
-    term
-    ]]
+        vim.cmd[[ term ]]
         tman.buf = vim.api.nvim_win_get_buf(win_id)
     else
         vim.api.nvim_win_set_buf(win_id, tman.buf)
