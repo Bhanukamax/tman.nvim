8f59daf119a1431cc31c86a415348bb37b223b68
Don Bhanuka <bhanukamax@gmail.com>
2023-02-12 20:58:48 +0530

    Added setup function

--- lua/tman.lua
+++ lua/tman.lua
@@ -2,8 +2,19 @@ local M = {}
 
 local tman = {}
 
+
 local pprint = function (tbl)
    vim.pretty_print(tbl)
+end
+
+tman.split = "horizontal"
+tman.width = 20
+tman.height =  10
+
+M.setup = function (tbl)
+    tman.split = tbl.split or "horizontal"
+    tman.width = tbl.width or 20
+    tman.height= tbl.height or 10
 end
 
 -- Goal is to drop harpoon as a dependency
@@ -24,19 +35,33 @@ end
 M.openTerm = function ()
     tman.last_buf_id = vim.api.nvim_get_current_buf()
     if M.isTermValid() then
-    vim.cmd[[
-    sp
-    wincmd J
-    ]]
+        if tman.split == "vertical" then
+            vim.cmd[[
+            sp
+        wincmd L
+            ]]
+        elseif tman.split == "horizontal" then
+            vim.cmd[[
+            sp
+            wincmd J
+            ]]
+        end
         vim.api.nvim_set_current_buf(tman.buf)
         return
     end
 
-    vim.cmd[[
-    sp
-    wincmd J
-    term
-    ]]
+        if tman.split == "vertical" then
+            vim.cmd[[
+            sp
+        wincmd L
+            ]]
+        elseif tman.split == "horizontal" then
+            vim.cmd[[
+            sp
+            wincmd J
+            ]]
+        end
+    vim.cmd[[ term ]]
     tman.win = vim.api.nvim_get_current_win()
     tman.buf = vim.api.nvim_win_get_buf(tman.win)
     tman.term = vim.b.terminal_job_id
@@ -99,9 +124,29 @@ M.closeTermIfOpen = function()
     return hasTerm
 end
 
+M.get_full_width = function ()
+    local full_width = 0
+   local wins = vim.api.nvim_list_wins()
+   for _,v in pairs(vim.api.nvim_list_wins()) do
+    full_width = full_width + vim.api.nvim_win_get_width(v)
+   end
+    vim.pretty_print({wins, full_width})
+   return full_width
+end
+
+
 M.toggleTerm = function ()
   if M.closeTermIfOpen() == false then
         M.openTerm()
+local width = M.get_full_width()
+    -- vim.pretty_print({'test'})
+
+    local win = vim.api.nvim_get_current_win()
+    if tman.split == "vertical" then
+        vim.api.nvim_win_set_width(win, math.floor(width /100 * tman.width ))
+    elseif tman.split == "horizontal" then
+        vim.api.nvim_win_set_height(win, math.floor(get_full_width() / tman.height))
+    end
   end
 end
 
